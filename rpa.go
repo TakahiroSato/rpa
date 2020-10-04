3425ca9cc83d3b0dffaf1e7dca6fac2ad7752ead
TakahiroSato <takahiro.sato@aijus.com>
2020-10-04 21:45:27 +0900

    ScaleFactorを外からオプションで渡すようにした

--- rpa.go
+++ rpa.go
@@ -9,33 +9,11 @@ import (
 	"github.com/lxn/win"
 )
 
-var scaleFactor float64
-
-func init() {
-
-	// 高DPIディスプレイ(4K)とかで、見づらいからWin10の設定で拡大率を100%から変更してると、robotgoでとってくる諸々の座標がずれちゃう
-
-	// GetScaleFactorForMonitorとGetScaleFactorForDevice試したけど何故かずれる（150がほしいのに140が来る）
-
-	// hwnd := robotgo.FindWindow("dollfro")
-	// hMonitor := win.MonitorFromWindow(hwnd, 2)
-	//libShcore := windows.NewLazySystemDLL("Shcore.dll")
-	// GetScaleFactorForMonitor := libShcore.NewProc("GetScaleFactorForMonitor")
-	// var s uint
-	// syscall.Syscall(GetScaleFactorForMonitor.Addr(), 2, uintptr(hMonitor), uintptr(unsafe.Pointer(&s)), 0)
-	// scaleFactor = float64(s) / 100.0
-
-	// getScaleFactorForDevice := libShcore.NewProc("GetScaleFactorForDevice")
-	// s, _, _ := syscall.Syscall(getScaleFactorForDevice.Addr(), 1, 2, 0, 0)
-
-	// 自分が150%に設定してるので、1.5固定で一旦設定
-	scaleFactor = 1.5
-}
-
 // SearchImgAndClickOpts : SearchImgAndClick関数のオプション引数
 type SearchImgAndClickOpts struct {
-	Tolerance float64
-	IsSaveImg bool
+	Tolerance   float64
+	IsSaveImg   bool
+	ScaleFactor float64
 }
 
 type option func(*SearchImgAndClickOpts)
@@ -54,11 +32,19 @@ func IsSaveImg(v bool) option {
 	}
 }
 
+// ScaleFactor : 画面の拡大率（デフォルトは1.5）
+func ScaleFactor(v float64) option {
+	return func(o *SearchImgAndClickOpts) {
+		o.ScaleFactor = v
+	}
+}
+
 // SearchImgAndClick : 指定タイトルのウィンドウから指定した画像(.png)を探しその場所をクリックする
 func SearchImgAndClick(title string, imgPath string, opts ...option) <-chan bool {
 	o := SearchImgAndClickOpts{
-		Tolerance: 0.01,
-		IsSaveImg: false,
+		Tolerance:   0.01,
+		IsSaveImg:   false,
+		ScaleFactor: 1.5,
 	}
 
 	for _, opt := range opts {
@@ -67,7 +53,7 @@ func SearchImgAndClick(title string, imgPath string, opts ...option) <-chan bool
 	ch := make(chan bool)
 
 	go func() {
-		wX, wY, wW, wH := getBounds(title)
+		wX, wY, wW, wH := getBounds(title, o.ScaleFactor)
 
 		printPreTime := func(str string) {
 			now := time.Now()
@@ -76,14 +62,14 @@ func SearchImgAndClick(title string, imgPath string, opts ...option) <-chan bool
 
 		var x, y int
 		// 1分くらい探して見つからなかったら無いと判断して終わる。
-		for i := 0; i < 6; i++ {
+		printPreTime("Finding " + imgPath + " ...")
+		for i := 0; i < 60; i++ {
 			result := func() bool {
-				printPreTime("Finding " + imgPath + " ...")
 				refRect := robotgo.CaptureScreen(wX, wY, wW, wH)
 				defer robotgo.FreeBitmap(refRect)
 				x, y = robotgo.FindPic(imgPath, refRect, o.Tolerance)
 				if x <= 0 || y <= 0 {
-					time.Sleep(time.Second * 10)
+					time.Sleep(time.Second * 1)
 					return false
 				}
 				return true
@@ -98,10 +84,10 @@ func SearchImgAndClick(title string, imgPath string, opts ...option) <-chan bool
 				go saveImg(title, wX, wY, wW, wH)
 			}
 
-			x = multiply(x, 1/scaleFactor)
-			y = multiply(y, 1/scaleFactor)
-			_wX := multiply(wX, 1/scaleFactor)
-			_wY := multiply(wY, 1/scaleFactor)
+			x = multiply(x, 1/o.ScaleFactor)
+			y = multiply(y, 1/o.ScaleFactor)
+			_wX := multiply(wX, 1/o.ScaleFactor)
+			_wY := multiply(wY, 1/o.ScaleFactor)
 
 			robotgo.MoveMouse(_wX+x, _wY+y)
 			robotgo.MouseClick("left", true)
@@ -133,7 +119,7 @@ func multiply(n int, mag float64) int {
 	return int(float64(n) * mag)
 }
 
-func getBounds(title string) (int, int, int, int) {
+func getBounds(title string, scaleFactor float64) (int, int, int, int) {
 	hwnd := robotgo.FindWindow(title)
 	if hwnd == 0 {
 		fmt.Fprintf(os.Stderr, "Could not find window")
--- rpa_test.go
+++ rpa_test.go
@@ -1,6 +1,7 @@
 package rpa
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,18 +11,44 @@ import (
 func Test_SearchImgAndClick(t *testing.T) {
 	title := "pwsh"
 	testImgPath := "./test/test.png"
-	wX, wY, wW, _ := getBounds(title)
-	findImg := robotgo.CaptureScreen(wX+wW-150, wY+10, 100, 100)
-	defer robotgo.FreeBitmap(findImg)
-	robotgo.SaveBitmap(findImg, testImgPath)
-	ch := SearchImgAndClick(title, testImgPath)
 
-	result := <-ch
-	if !result {
-		t.Error("NG")
+	scaleFactors := [7]float64{
+		1.0,
+		1.25,
+		1.5,
+		1.75,
+		2.0,
+		2.25,
+		2.5,
 	}
 
-	if err := os.Remove(testImgPath); err != nil {
-		t.Error(err)
+	results := make([]bool, 7)
+
+	for i, scaleFactor := range scaleFactors {
+		wX, wY, wW, _ := getBounds(title, 1.25)
+		findImg := robotgo.CaptureScreen(wX+wW-150, wY+10, 100, 100)
+		defer robotgo.FreeBitmap(findImg)
+		robotgo.SaveBitmap(findImg, testImgPath)
+		ch := SearchImgAndClick(title, testImgPath, ScaleFactor(scaleFactor))
+
+		results[i] = <-ch
+
+		if err := os.Remove(testImgPath); err != nil {
+			t.Error(err)
+		}
+	}
+
+	testResult := false
+	for i, r := range results {
+		if r {
+			fmt.Printf("OK %0.2f\n", scaleFactors[i])
+			testResult = true
+		} else {
+			fmt.Printf("NG %0.2f\n", scaleFactors[i])
+		}
+	}
+
+	if !testResult {
+		t.Error("NG")
 	}
 }
